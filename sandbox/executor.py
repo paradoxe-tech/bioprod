c1e3afbb0b92fd82d4d29017ec5e465981c5fb06
paradoxe-tech <89200698+paradoxe-tech@users.noreply.github.com>
2025-05-17 12:49:44 +0200

    ensured docker container is conserved

--- sandbox/executor.py
+++ sandbox/executor.py
@@ -1,5 +1,6 @@
 import docker
-from typing import Dict, List, Union
+from typing import Dict, List, Union, Optional
+from docker.models.containers import Container
 
 class DockerExecutor:
     
@@ -13,23 +14,25 @@ class DockerExecutor:
             "cpu_period": config.get("cpu_period", 100000),
             "cpu_quota": config.get("cpu_quota", 50000),
         }
+
+        self.container: Optional[Container] = None
+        self.start()
     
     def run(self, command: Union[str, List[str]]) -> Dict:
         try:
-            output = self.client.containers.run(
-                image=self.image,
-                command=command,
-                stdout=True,
-                stderr=True,
-                remove=self.constraints["remove"],
-                network_disabled=self.constraints["network_disabled"],
-                mem_limit=self.constraints["mem_limit"],
-                cpu_period=self.constraints["cpu_period"],
-                cpu_quota=self.constraints["cpu_quota"],
-            )
+            if self.container is None:
+                return {
+                    "success": False,
+                    "output": None,
+                    "error": "Container not started"
+                }
+            
+            exec_log = self.container.exec_run(command)
+            output = exec_log.output.decode()
+
             return {
                 "success": True,
-                "output": output.decode(),
+                "output": output,
                 "error": None
             }
         except Exception as e:
@@ -38,3 +41,16 @@ class DockerExecutor:
                 "output": None,
                 "error": str(e)
             }
+        
+    def start(self):
+        if self.container is None:
+            self.container = self.client.containers.run(
+                image=self.image,
+                command="sleep infinity",
+                detach=True,
+                remove=self.constraints["remove"],
+                network_disabled=self.constraints["network_disabled"],
+                mem_limit=self.constraints["mem_limit"],
+                cpu_period=self.constraints["cpu_period"],
+                cpu_quota=self.constraints["cpu_quota"],
+            )
